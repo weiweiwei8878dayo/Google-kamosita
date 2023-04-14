359365e0e7a5fc1b8696a2f0e87e8241439d6c4d
NUTAD <86157431+weiweiwei8878dayo@users.noreply.github.com>
2023-04-14 21:50:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-document.getElementById('search-form').addEventListener('submit', search);
-
+// 検索フォームのイベントハンドラ
 function search() {
-    const query = document.getElementById('query').value;
-    fetch(`/search?query=${query}`)
-        .then(response => response.json())
-        .then(results => displayResults(results));
-}
+    // 検索結果をリセット
+    document.getElementById("results").innerHTML = "";
 
-function displayResults(results) {
-    const resultsList = document.getElementById('results');
-    resultsList.innerHTML = '';
-    results.forEach(result => {
-        const li = document.createElement('li');
-        li.textContent = result;
-        resultsList.appendChild(li);
-    });
+    // 入力されたキーワードを取得
+    var query = document.getElementById("query").value;
+
+    // 検索結果の取得と表示
+    if (query) {
+        // Google風の検索結果を生成
+        var results = [
+            {
+                title: "検索結果1",
+                url: "https://www.example.com/result1"
+            },
+            {
+                title: "検索結果2",
+                url: "https://www.example.com/result2"
+            },
+            {
+                title: "検索結果3",
+                url: "https://www.example.com/result3"
+            }
+        ];
+
+        // 検索結果をリスト形式で表示
+        var resultsList = document.getElementById("results");
+        results.forEach(function(result) {
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+            a.textContent = result.title;
+            a.href = result.url;
+            li.appendChild(a);
+            resultsList.appendChild(li);
+        });
+    }
 }
